3b0283567057f75711f0f8a77eba00b905ab6ecc
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2020-07-27 00:53:13 +0530

    Update

--- threshold/lib/buttons.dart
+++ threshold/lib/buttons.dart
@@ -503,7 +503,7 @@ class _PointifyProjectButtonState extends State<PointifyProjectButton> {
       child: GestureDetector(
         onTap: () {
           launch(
-              "https://play.google.com/store/apps/details?id=com.app2.farmringg&hl=en");
+              "https://play.google.com/store/apps/details?id=com.rr.pointify");
         },
         child: AnimatedContainer(
           duration: Duration(milliseconds: 400),
@@ -720,6 +720,89 @@ class _ArenaProjectButtonState extends State<ArenaProjectButton> {
   }
 }
 
+class ThresholdProjectButton extends StatefulWidget {
+  @override
+  _ThresholdProjectButtonState createState() => _ThresholdProjectButtonState();
+}
+
+class _ThresholdProjectButtonState extends State<ThresholdProjectButton> {
+  List<Color> colors = [beigeColor, beigeColor];
+  Offset lightShadow = Offset(-5, -5);
+  Offset darkShadow = Offset(5, 5);
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          lightShadow = Offset(0, 0);
+          darkShadow = Offset(0, 0);
+          colors = [
+            //    Color.fromRGBO(255, 254, 239, 1),
+            Color.fromRGBO(230, 213, 201, 1),
+            Color.fromRGBO(250, 239, 232, 1),
+          ];
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          lightShadow = Offset(-5, -5);
+          darkShadow = Offset(5, 5);
+          colors = [beigeColor, beigeColor];
+        });
+      },
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {
+          launch("https://github.com/rushilrai/Threshold");
+        },
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 400),
+          height: displayWidth(context) * 0.025,
+          width: displayWidth(context) * 0.12,
+          decoration: BoxDecoration(
+            color: beigeColor,
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: colors,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: lightshadowColor,
+                offset: lightShadow,
+                blurRadius: 10,
+              ),
+              BoxShadow(
+                color: darkshadowColor,
+                offset: darkShadow,
+                blurRadius: 10,
+              ),
+            ],
+            borderRadius: BorderRadius.all(Radius.circular(17)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  'View Project',
+                  style: TextStyle(
+                    color: brownColor,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w200,
+                    fontSize: displayWidth(context) * 0.012,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class ThresholdDetailsButton extends StatefulWidget {
   @override
   _ThresholdDetailsButtonState createState() => _ThresholdDetailsButtonState();
@@ -753,7 +836,144 @@ class _ThresholdDetailsButtonState extends State<ThresholdDetailsButton> {
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
         onTap: () {
-          launch("tel:+919650693092");
+          showMaterialModalBottomSheet(
+            enableDrag: false,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            )),
+            context: context,
+            builder: (context, scrollController) => Container(
+              height: displayHeight(context) * 0.9,
+              decoration: BoxDecoration(
+                color: beigeColor,
+              ),
+              child: CenteredView(
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.arrow_back_ios,
+                            color: brownColor,
+                            size: displayWidth(context) * 0.025,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Threshold',
+                              style: TextStyle(
+                                color: brownColor,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w200,
+                                fontSize: displayWidth(context) * 0.025,
+                              ),
+                            ),
+                            TweenAnimationBuilder(
+                              duration: Duration(milliseconds: 1000),
+                              tween: Tween<double>(
+                                begin: 0,
+                                end: displayWidth(context) * 0.1,
+                              ),
+                              builder: (context, value, child) {
+                                return AnimatedContainer(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: brownColor,
+                                  ),
+                                  duration: Duration(milliseconds: 200),
+                                  width: value,
+                                  height: 2.5,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Threshold, the name of my first Portfolio Website. \nIt features all my Projects and Work, and also a few things you would like to know about me. \nTried to experiment with a different design as compared to most Pother Portfolio websites.",
+                      style: TextStyle(
+                        color: aquaColor,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w200,
+                        fontSize: displayWidth(context) * 0.02,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ThresholdProjectButton(),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: displayHeight(context) * 0.8,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            alignment: Alignment.topCenter,
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                                'assets/images/workbg/threshold/thresholddetails1.png'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: displayHeight(context) * 0.8,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                                'assets/images/workbg/threshold/thresholddetails2.png'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: displayHeight(context) * 0.8,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            alignment: Alignment.bottomCenter,
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                                'assets/images/workbg/threshold/thresholddetails3.png'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
         },
         child: TweenAnimationBuilder(
           tween: Tween<double>(
